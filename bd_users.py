8f1175986c4d5dae7cef834d2ea8549958ab9783
Your oni-chan <my@python.com>
2019-04-26 00:13:56 +0300

    можно я кого-нибудь убью

--- bd_users.py
+++ bd_users.py
@@ -24,7 +24,8 @@ class UserModel:
                             (id INTEGER PRIMARY KEY AUTOINCREMENT,
                              user_id INTEGER, 
                              user_name VARCHAR(16),
-                             dialog INTEGER
+                             dialog INTEGER,
+                             wiki INTEGER
                              )''')
         cursor.close()
         self.connection.commit()
@@ -32,9 +33,9 @@ class UserModel:
     def insert(self, user_id, user_name, dialog):
         cursor = self.connection.cursor()
         cursor.execute('''INSERT INTO users 
-                          (user_id, user_name, dialog) 
-                          VALUES (?,?,?)''', 
-                          (user_id, user_name, dialog))
+                          (user_id, user_name, dialog, wiki) 
+                          VALUES (?,?,?,?)''', 
+                          (user_id, user_name, dialog, 0))
         logging.info(f'Новый пользователь {user_name}(id={user_id})')
         cursor.close()
         self.connection.commit()
@@ -49,10 +50,42 @@ class UserModel:
             row = self.get(user_id)
         return(row)
     
+    def new_dialog(self, id_1, id_2):
+        cursor = self.connection.cursor()
+        cursor.execute('''UPDATE users SET dialog = ? WHERE user_id = ?''',(id_2,id_1))
+        cursor.execute('''UPDATE users SET dialog = ? WHERE user_id = ?''',(id_1,id_2))
+        cursor.close()
+        self.connection.commit()
+
+    def stop_dialog(self, id_1, id_2):
+        cursor = self.connection.cursor()
+        cursor.execute('''UPDATE users SET dialog = ? WHERE user_id = ?''',(0,id_1))
+        cursor.execute('''UPDATE users SET dialog = ? WHERE user_id = ?''',(0,id_2))
+        cursor.close()
+        self.connection.commit()
+
+    def wiki(self, user_id, logic):
+        cursor = self.connection.cursor()
+        cursor.execute('''UPDATE users SET wiki = ? WHERE user_id = ?''',(logic,user_id))
+        cursor.close()
+        self.connection.commit()
+ 
     def delete_user(self, user_id):
         cursor = self.connection.cursor()
         self.get(user_id)
         cursor.execute('''DELETE FROM users WHERE id = ?''',
                          (str(user_id),))
         cursor.close()
-        self.connection.commit()
+        self.connection.commit()    
+
+    #не работает
+    def free_people(self):
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE dialog = ?", (0,))
+        row = cursor.fetchone()
+        print(row)
+        print(len(row))
+        if row is None:
+            return 'все заняты'
+        else:
+            return row
--- main.py
+++ main.py
@@ -20,46 +20,96 @@ logging.basicConfig(filename='info.log',
 
 
 def main():
-    wiki = False
     UserModel(db.get_connection()).init_table()
     user_model = UserModel(db.get_connection())
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             info = user_information(event.user_id)
             user = user_model.get(info['id'])
-            # (id, user_id, Имя, id собеседника (0, если его нет))
+            # (id, user_id, Имя, id собеседника (0, если его нет), значение wiki 0 или 1)
             print(user)
             all_request = event.text.lower()
             all_request = "".join(l for l in all_request if l not in string.punctuation) 
             request = all_request.split()
-            logging.debug('{} прислал сообщение {}'.format(info['first_name'], request))
+            logging.info('{} прислал сообщение {}'.format(info['first_name'], request))
             print(request)
             response = []
-            for fraze in TALK:
-                if fraze in all_request:
-                    pass
-            if wiki:
-                response.append('Вот что я нашёл: \n' + str(wikipedia.summary(event.text)))
-                wiki = False
+            if 'vk.com/' in event.text:
+                    start = event.text.find('vk.com/') + 7
+                    print(event.text[start:])
+                    name = event.text[start:].split()[0]
+                    print(name)
+                    person_id = id_by_name(name)
+                    if person_id == 'error':
+                        write_msg(event.user_id, 'id пренадлежит не пользователю')
+                    else:
+                        info_2 = user_information(person_id)
+                        user_2 = user_model.get(info_2['id'])
+                        if user_2[3] != 0:
+                            write_msg(event.user_id, 'Собеседник уже сосотоит в диалоге')
+                            continue
+                        if user[3] != 0:
+                            user_model.stop_dialog(event.user_id, user[3])
+                            write_msg(info['id'], 'Диалог завершён')
+                            write_msg(user[4], 'Диалог завершён')
+                        user_model.new_dialog(info['id'], info_2['id'])
+                        write_msg(info['id'], 'Начат диалог')
+                        write_msg(info_2['id'], 'Начат диалог')
+                        continue
+            elif user[3] != 0:
+                info_2 = user_information(user[3])
+                user_2 = user_model.get(info_2['id'])
+                for word in STOP_DIALOG:
+                    if word in all_request:
+                        user_model.stop_dialog(user[0], user[3])
+                        break
+                else:
+                    write_msg(user[3], event.text)
+                    logging.info('{} получил сообщение от {}'.format(info_2['first_name'] + ' ' + info_2['last_name'],
+                                                                     info['first_name'] + ' ' + info['last_name']))
             else:
+                if user[4]:  # значение wiki 0 или 1
+                    try:
+                        write_msg(event.user_id,'Вот что я нашёл: \n' + str(wikipedia.summary(event.text)))
+                        wiki = False
+                    except:
+                        write_msg(event.user_id,'По запросу ничего не найдено')
+                    user_model.wiki(info['id'], 0)
+                    continue
                 for word in request:
                     if word in HELLO:
                         response.append(random.choice(HELLO) + ', ' + info['first_name'])
                     if word == "клава":
                         keyboard_vk(event.user_id)
                         response.append("..")
+                    if word in HELP:
+                        keyboard_vk(event.user_id)
+                        helps = open('help.txt', 'r')
+                        stroka = ''
+                        for st in  helps.readlines():
+                            stroka += st[:-1]
+                        response.append(stroka)
+                        helps.close()
+                    if word == "погода":
+                        weather_vk(event.user_id)
+                        response.append(".^_^")
                     if word == "вики":
-                        wiki = True
+                        user_model.wiki(info['id'], 1)
                         response.append("Введите запрос")
-                    if word == "пока" and len(response) == 0:
-                        response.append("Пока")
+                    if word == 'спасибо':
+                        response.append("Пожалуйста")
+                    if (word == "пока" or word == "прощай") and len(response) == 0:
+                        response.append("Пока. Надеюсь больше тебя не услышу")
+                        if user[3] != 0:
+                            user_model.wiki(user[3], 0)
+                            write_msg(user[3], 'Собеседник самовыпилился. Приносим его извинения')
                         user_model.delete_user(event.user_id)
                         logging.info('Завершение диалога с {}(id={})'.format(info['first_name'], info['id']))
                     if len(response) == 0:
                         response.append("Не понялa вашего ответа..")
-            response = map(lambda x: x[0].upper() + x[1:] + '. ', response)
-            response = ''.join(response)
-            write_msg(event.user_id, response) 
+                response = map(lambda x: x[0].upper() + x[1:] + '. ', response)
+                response = ''.join(response)
+                write_msg(event.user_id, response) 
 
 
 if __name__ == '__main__':
